49850997b1607b616e271d3478f86b2a5444c48c
Iván A. <hello@alleksy.com>
2025-10-10 11:53:36 -0700

    Restore sidebar section highlighting when open

--- components/ui/sidebar-link-group.tsx
+++ components/ui/sidebar-link-group.tsx
@@ -21,7 +21,7 @@ export default function SidebarLinkGroup({
   }
 
   return (
-    <li className="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 group is-link-group">
+    <li className={`pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-linear-to-r group is-link-group ${openGroup && 'from-indigo-600/[0.12] dark:from-indigo-600/[0.24] to-indigo-600/[0.04]'}`}>
       {children(handleClick, openGroup)}
     </li>
   )
