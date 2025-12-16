1d13eb49cc33f27fc7d67bcc8d68acbf45ef2c94
Your Name <hello@alleksy.com>
2025-12-16 14:13:17 -0800

    fixed 3?

--- components/charts/bar-chart-02.tsx
+++ components/charts/bar-chart-02.tsx
@@ -95,7 +95,7 @@ export default function BarChart02({
           tooltip: {
             callbacks: {
               title: () => '', // Disable tooltip title
-              label: (context) => formatValue(context.parsed.y),
+              label: (context) => formatValue(context.parsed.y ?? 0),
             },
             bodyColor: darkMode ? tooltipBodyColor.dark : tooltipBodyColor.light,
             backgroundColor: darkMode ? tooltipBgColor.dark : tooltipBgColor.light,
